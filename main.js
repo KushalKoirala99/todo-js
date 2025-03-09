dfdfca25a464fc6d56716a15f1f771a6ced70e7a
kushal <kushalkoirala58@gmail.com>
2025-03-09 13:03:40 +0545

    Fix(ui) : correct listStyle logic for empty and notEmpty state

--- main.js
+++ main.js
@@ -1,120 +1,129 @@
-const todoInput = document.querySelector('#todoTask');
+const todoInput = document.querySelector("#todoTask");
 const btnAdd = document.querySelector(`#addBtn`);
 const list = document.querySelector(`#todoList`);
 
-
-
-
 class Todo {
-    constructor(task){
-        this.task = task;
-        this.id = crypto.randomUUID();
-    }
+  constructor(task) {
+    this.task = task;
+    this.id = crypto.randomUUID();
+  }
 
-    static deleteTodo(id){
-        const todoArray = getFromLocalStorage();
-        const updatedArray = todoArray.filter(todo => todo.id !== id);
-        addToLocalStorage(updatedArray);
-        renderTodos();
-    }
+  static deleteTodo(id) {
+    const todoArray = getFromLocalStorage();
+    const updatedArray = todoArray.filter((todo) => todo.id !== id);
+    addToLocalStorage(updatedArray);
+    renderTodos();
+  }
 }
 
-let userInput = '';
+let userInput = "";
 
-
-
-btnAdd.addEventListener('click',()=>{
-    if(userInput !== ""){
-        addTodo(userInput);
-        todoInput.value = "";
-        renderTodos()
-    }
+btnAdd.addEventListener("click", () => {
+  if (userInput !== "") {
+    addTodo(userInput);
+    todoInput.value = "";
+    renderTodos();
+  }
 });
 
-document.addEventListener('DOMContentLoaded', renderTodos);
+document.addEventListener("DOMContentLoaded", () => {
+  renderTodos();
+  listStyle();
+});
 
-todoInput.addEventListener('change',(e) => {
-      userInput =  e.target.value;
-})
+todoInput.addEventListener("change", (e) => {
+  userInput = e.target.value;
+});
 
+function addTodo(task) {
+  const todo = new Todo(task);
+  const todoArray = getFromLocalStorage();
+  todoArray.push(todo);
+  addToLocalStorage(todoArray);
+  userInput = "";
+}
 
- function addTodo (task){
-    const todo = new Todo(task);
-    const todoArray = getFromLocalStorage();
-    todoArray.push(todo);
-    addToLocalStorage(todoArray);
-    userInput = "";
- }
+function renderTodos() {
+  list.innerHTML = "";
+  let todos = getFromLocalStorage();
+  todos.map((item) => {
+    let todoElement = createElements(item);
+    list.appendChild(todoElement);
+  });
 
+  listStyle();
+}
 
- function renderTodos(){
-     list.innerHTML = "";
-     let todos = getFromLocalStorage()
-     todos.map((item) =>{
-      let todoElement = createElements(item);
-      list.appendChild(todoElement);
-     })
- }
+function addToLocalStorage(item) {
+  return localStorage.setItem("todos", JSON.stringify(item));
+}
 
- function addToLocalStorage(item){
-return localStorage.setItem('todos',JSON.stringify(item))
- }
+function getFromLocalStorage() {
+  const storedData = localStorage.getItem("todos");
+  return storedData ? JSON.parse(storedData) : [];
+}
 
- function getFromLocalStorage(){
-    const storedData = localStorage.getItem('todos');
-    return storedData ? JSON.parse(storedData) : [];
- };
+function createElements(todo) {
+  const li = document.createElement("li");
+  const div = document.createElement("div");
+  div.innerText = todo.task;
 
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "x";
+  delBtn.classList.add("del-btn");
 
- function createElements(todo){
-   const li = document.createElement('li');
-   const div = document.createElement('div');
-   div.innerText = todo.task;
+  delBtn.addEventListener("click", () => Todo.deleteTodo(todo.id));
 
-   const delBtn = document.createElement('button');
-   delBtn.innerText = 'x';
-   delBtn.classList.add('del-btn')
+  li.appendChild(div);
+  li.append(delBtn);
+  editTask(div, todo);
 
-   delBtn.addEventListener('click', () => Todo.deleteTodo(todo.id))
+  return li;
+}
 
-   li.appendChild(div)
-   li.append(delBtn);
-   editTask(div,todo)
+function editTask(div, todo) {
+  div.addEventListener("dblclick", () => {
+    const input = document.createElement("input");
+    input.type = "text";
+    input.classList = "edit";
+    input.value = div.innerText;
 
-   return li
- }
+    div.replaceWith(input);
+    input.focus();
 
- function editTask(div, todo){
-    div.addEventListener('dblclick',()=>{
-       const input = document.createElement('input');
-       input.type  = 'text';
-       input.value = div.innerText;
+    input.addEventListener("blur", () => {
+      replaceInput(input, todo);
+    });
 
-       div.replaceWith(input);
-       input.focus();
+    input.addEventListener("keydown", (e) => {
+      if (e.key === "Enter") {
+        replaceInput(input, todo);
+      }
+    });
+  });
+}
 
-       input.addEventListener('blur',()=>{
-        replaceInput(input,todo);
-       })
+function replaceInput(input, todo) {
+  const updateText = input.value.trim();
 
-       input.addEventListener('keydown',(e)=>{
-           if(e.key === 'Enter'){
-            replaceInput(input,todo)
-           }
-       })
-    })
+  if (updateText === "") return;
 
- }
+  let todos = getFromLocalStorage();
+  const updateTodos = todos.map((i) =>
+    i.id === todo.id ? { ...i, task: updateText } : i
+  );
+  addToLocalStorage(updateTodos);
 
+  renderTodos();
+}
 
- function replaceInput(input,todo){
- const updateText = input.value.trim();
-
- if(updateText === '' )return;
-
- let todos = getFromLocalStorage();
- const updateTodos = todos.map((i) => i.id === todo.id ? {...i, task: updateText} : i)
- addToLocalStorage(updateTodos);
-
- renderTodos();
- }
+function listStyle() {
+  let todos = getFromLocalStorage();
+  if (todos.length > 0) {
+    list.classList.add("notEmpty");
+    list.classList.remove("empty");
+  } else {
+    list.classList.add("empty");
+    list.classList.remove("notEmpty");
+  }
+}
